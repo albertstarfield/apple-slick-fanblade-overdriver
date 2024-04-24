b13cf49777d977a5de93276f8e655506093f152b
Albert Starfield Wahyu Suryo S <albertstarfield2001@gmail.com>
2024-04-25 06:09:20 +0700

    Init Commit

---
+++ enableOverdrive.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./smc -k F0Fb -w 01; ./smc -k F0Dc -w 4eab2c3f; ./smc -k F0Md -w 01; ./smc -k F0St -w 05; ./smc -k F0Tg -w 0080d449; ./smc -k F1Fb -w 01; ./smc -k F1Dc -w 4eab2c3f; ./smc -k F1Md -w 01; ./smc -k F1St -w 05; ./smc -k F1Tg -w 0080d449
